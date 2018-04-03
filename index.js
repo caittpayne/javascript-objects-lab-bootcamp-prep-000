e8337f92a449e024ae5d6d4ff69e8fe0c2f9284e
Cait Payne <caitpayne@outlook.com>
2018-04-03 22:57:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value });
 }
 
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value; 
   
   return object;
+}
+
+
+function deleteFromObjectByKey(object, key) {
+  return
 }
